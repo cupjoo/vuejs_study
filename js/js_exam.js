4482a9fd1238da6b3b8d96d8d66dd68c2a5fd541
Unknown <cupjoo@naver.com>
2018-08-13 19:41:25 +0900

    배열 관련 함수 추가

--- js/js_exam.js
+++ js/js_exam.js
@@ -6,3 +6,13 @@ function print_exam(){
     document.write((a===b)+'<br>');  // 데이터형도 비교 (부정형은 !==)
     console.log('로그 출력 테스트');
 }
+
+// 배열 관련 예제
+function arr_exam(){
+    var arr = [1,2,3];
+    arr.pop();
+    arr.push(4);
+    for(i in arr){
+        document.write(arr[i]+'<br>');
+    }
+}
