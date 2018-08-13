83d052fd2c6d5f57540bc32584de8a3e599bd2bf
Unknown <cupjoo@naver.com>
2018-08-14 03:25:13 +0900

    element 제어 로직 추가

--- js/jquery_exam2.js
+++ js/jquery_exam2.js
@@ -16,3 +16,30 @@ $('#trigger_event').on('click', function () {
 $('#remove_event').on('click', function () {
     $('#click').off('click', clickHandler)
 });
+
+// start of element 제어
+
+// 부모, 형제 추가
+$('#intro').append('Junyoung').after('<div>What\'s your name?</div>');
+// 부모로 감싸기
+$('#wrapping').wrap('<div id="first_wrap"><strong id="second_wrap"></strong></div>');
+// 삭제
+$('#delete_el').on('click', function () {
+    $('#wrapping').remove();
+});
+// 교체
+$('#replace_el').on('click', function () {
+    $('<div>replaced</div>').replaceAll('p');
+});
+
+// 클래스 제어
+$('#first').click(function () {
+    $(this).toggleClass('highlight');
+});
+// 속성 제어
+$('input').keyup(function () {
+    var value = $(this).val();
+    $('#result').text(value);
+});
+
+// end of element 제어
