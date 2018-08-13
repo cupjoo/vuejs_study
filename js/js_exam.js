c6e67169bc298f281450628368c479bbe458f571
Unknown <cupjoo@naver.com>
2018-08-13 19:41:10 +0900

    js 파일 생성
    출력 관련 함수 생성

---
+++ js/js_exam.js
@@ -0,0 +1,8 @@
+// 출력 관련 예제
+function print_exam(){
+    var a =5, b='5';
+
+    document.write((a==b)+'<br>');   // 값 비교
+    document.write((a===b)+'<br>');  // 데이터형도 비교 (부정형은 !==)
+    console.log('로그 출력 테스트');
+}
