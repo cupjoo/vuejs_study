38d5a0dd1a6f4b7b326bce837426ee0414be88c0
Unknown <cupjoo@naver.com>
2018-08-13 21:48:11 +0900

    주석 추가

--- js/jquery_exam.js
+++ js/jquery_exam.js
@@ -1,5 +1,9 @@
 // jQuery 사용 예제
+
+
+// $(element|selector).method1().method2()
 $('.welcome').html('hello word!').css('background-color','yellow');
+
 $('#navigation li').on('click', function() {
     $('#navigation li').removeClass('selected');
     $(this).addClass('selected');
