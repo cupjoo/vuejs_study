169481786dd6005b2ad42e1ab9251fc818aceec3
Unknown <cupjoo@naver.com>
2018-08-14 19:27:33 +0900

    콘솔 로그 변경

--- js/vue-module/1-vue-instance.js
+++ js/vue-module/1-vue-instance.js
@@ -1,22 +1,44 @@
+// Vue Instance
+
+/* Instance */
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue.js!'
+        my_message: 'Hello Vue.js!'
     },
     beforeCreate: function(){
-        console.log('beforeCreate');
+        // Life Cycle Hook (사용자 추가 로직)
+        console.log('beforeCreate', this.my_message);
     },
     created: function(){
-        console.log('created');
+        console.log('created', this.my_message);
     },
     mounted: function(){
-        console.log('mounted');
-        this.message = 'changed message'
+        console.log('mounted', this.my_message);
+        this.my_message = 'Goodbye Vue.js!'
     },
     updated: function(){
-        console.log('updated');
+        console.log('updated', this.my_message);
     },
     destroyed: function(){
-        console.log('destroyed');
+        console.log('destroyed', this.my_message);
     }
-});
+}); /* Instance */
+
+
+/*
+* Vue Instance Life Cycle
+*
+* beforeCreate
+* created
+*
+* beforeMount
+* mounted
+*
+* beforeUpdate
+* updated
+*
+* beforeDestroy
+* destroyed
+*
+*/
