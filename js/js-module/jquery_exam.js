c007921af0981de441ec25479f4ab480ece8bee9
Unknown <cupjoo@naver.com>
2018-08-14 16:04:26 +0900

    클래스 제어 로직 위치 수정

--- js/js-module/jquery_exam.js
+++ js/js-module/jquery_exam.js
@@ -8,11 +8,21 @@ $('.welcome').html('hello word!').css('background-color','yellow');
 $('#navigation li').on('click', function() {
     $('#navigation li').removeClass('selected');
     $(this).addClass('selected');
-})
+});
+
+// 클래스 토글
+$('#first').click(function () {
+    $(this).toggleClass('highlight');
+});
+
+// 속성 제어
+$('input').keyup(function () {
+    var value = $(this).val();
+    $('#result').text(value);
+});
 
 // chain - jQuery 메소드는 자기 자신을 반환한다.
 $('#my_link').attr('href', 'https://naver.com').attr('target', '_black').css('color', 'red');
 
 // traverse - chain의 대상을 바꿔 체인을 연장시키는 방법
 $('#family').find('#father').css('background-color', 'green').end().find('#brother').css('background-color', 'gray');
- 
