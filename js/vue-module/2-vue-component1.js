ba57c660927fedc9c7e45140d1177f625399dbfa
Unknown <cupjoo@naver.com>
2018-08-14 22:37:55 +0900

    지역 컴포넌트 호출 방식 변경

--- js/vue-module/2-vue-component1.js
+++ js/vue-module/2-vue-component1.js
@@ -1,19 +1,22 @@
-// 전역 component 등록
+// 전역 & 지역 Component
+
+
+// 전역 component 등록 - 어디서든 해당 component를 사용할 수 있음
 Vue.component('my-global-component', {
     template: '<div>전역 컴포넌트가 등록되었습니다!</div>'
 });
-// 지역 component 내용
-var cmp = {
-    template: '<div>지역 컴포넌트가 등록되었습니다!</div>'
-};
+
 // first instance
 new Vue({
     el: '#app',
-    // 지역 component 등록
+    // 지역 component 등록 - 현재 인스턴스에서만 component를 사용할 수 있음
     components: {
-        'my-local-component': cmp
+        'my-local-component': {
+            template: '<div>지역 컴포넌트가 등록되었습니다!</div>'
+        }
     }
 });
+
 // second instance
 new Vue({
     el: '#app2'
