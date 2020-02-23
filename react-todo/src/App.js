1c62a5969e255f4f4487dac403dd66ee2f74cde0
cupjoo <cupjoo@naver.com>
2020-02-23 15:19:01 +0900

    add react-todo app

---
+++ react-todo/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import TodoListTemplate from './components/TodoListTemplate'
+
+export default class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
+    )
+  }
+}
---
+++ react-todo/src/components/TodoListTemplate.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './TodoListTemplate.css'
+
+export default class TodoListTemplate extends Component {
+  render() {
+    const {form, children} = this.props
+    return (
+      <main className="todo-list-template">
+        <div className="title">
+          오늘 할 일
+        </div>
+        <section className="form-wrapper">
+          {form}
+        </section>
+        <section className="todos-wrapper">
+          { children }
+        </section>
+      </main>
+    )
+  }
+}
