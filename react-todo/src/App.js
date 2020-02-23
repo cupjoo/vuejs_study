6dff3b99a45b9bcf4e971905c0284e5745ab1f8d
cupjoo <cupjoo@naver.com>
2020-02-23 22:57:26 +0900

    add todoItemList

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import TodoListTemplate from './components/TodoListTemplate'
 import Form from './components/Form'
+import TodoItemList from './components/TodoItemList'
 
 export default class App extends Component {
   render() {
     return (
       <TodoListTemplate form={<Form/>}>
-        템플릿 완성
+        <TodoItemList/>
       </TodoListTemplate>
     )
   }
