90e5af4da784e1299d577933d95681990bd7a656
cupjoo <cupjoo@naver.com>
2020-02-23 23:27:28 +0900

    add Todo actions

--- react-todo/src/components/TodoItemList.js
+++ react-todo/src/components/TodoItemList.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import TodoItem from './TodoItem'
 
 export default class TodoItemList extends Component {
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+  
   render() {
     const { todos, onToggle, onRemove } = this.props
+    const todoList = todos.map(
+      (todo) => (
+        <TodoItem
+          {...todo}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={todo.id}          
+        />
+      )
+    )
     return (
       <div>
-        <TodoItem text="안녕"/>
-        <TodoItem text="리액트"/>
-        <TodoItem text="반가워"/>
+        {todoList}
       </div>
     )
   }
