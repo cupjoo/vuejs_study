5d7a3953e62ea75665c89f49d1c85db9c7bd1982
Unknown <cupjoo@naver.com>
2020-05-06 23:20:20 +0900

    add subscribe on display comp

--- redux-react/src/components/DisplayNumber.js
+++ redux-react/src/components/DisplayNumber.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import store from "../store";
 
 const DisplayNumber = () => {
+  const [number, setNumber] = useState(store.getState().number)
+  store.subscribe(() => setNumber(store.getState().number))
+
   return (
     <div>
       <h1>Display Number</h1>
-      <input type="text" value="0" readOnly></input>
+      <input type="text" value={number} readOnly></input>
     </div>
   );
 };
